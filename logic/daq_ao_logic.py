84222dc0bc1aced129acbb10eb6fce1b517beb4e
fbarho <fbarho@pop-os.localdomain>
2020-11-17 14:34:58 +0100

    some tests on this module
    not yet operational

--- logic/daq_ao_logic.py
+++ logic/daq_ao_logic.py
@@ -26,14 +26,16 @@ class DAQaoLogic(GenericLogic):
     """
 
     # declare connectors
-    #daq = Connector(interface='DAQInterface')
+    daq = Connector(interface='DAQInterface')
 
 
  
     
     
     # private attributes
-    
+    #_intensity_value = 0 # for testing do as if there is only one laser
+    _intensity_dict = {}
+    _channel_dict = {} # see how constraints dicts are passed to logic modules.. this should be similar here. 
 
 
 
@@ -47,25 +49,38 @@ class DAQaoLogic(GenericLogic):
         """
         #self._daq = self.daq()
         
-        self.enabled = False # attribute to handle the on-off switching 
+        self.enabled = False # attribute to handle the on-off switching of the laser-on button
 
 
     def on_deactivate(self):
         """ Perform required deactivation. """
         pass
 
-    def apply_voltage(self): #voltage, channel
+    
+    def apply_voltage(self): #channel, voltage
         
         self.enabled = True
-        #self._daq.apply_voltage(voltage, channel)
+        self._daq.set_voltage('/Dev1/AO0', self._intensity_dict['Laser1'])
+        ## something like [self._daq.set_voltage(self._channel_dict[laser], self._intensity_dict[laser]) for laser in self._intensity_dict]
+        # have to link laser names to channels in config and create a dictionary from this ### to be fixed !!!!!!!!!
+        # does the delay due to iteration matter ? (laser 4 switched on slightly after laser 1 ..)
     
     def voltage_off(self): # channel
         
         self.enabled = False
         #self._daq.apply_voltage(0.0, channel)
+      
+#    @QtCore.Slot(int)  
+#    def update_intensity_value(self, value):
+#        self._intensity_value = value
 
 
+    @QtCore.Slot(str, int)  
+    def update_intensity_dict(self, key, value):
+        self._intensity_dict[key] = value
 
+
+ ## check when intensity should be used and when it is converted into voltage .. the user indicates intensity ??? or a percentage of max voltage ????
  
 
 
