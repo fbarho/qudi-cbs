da379dd2f34771a1efab668deb8cb30e5aaa7128
fbarho <franziska.barho@gmx.fr>
2020-12-01 14:32:36 +0100

    added a multichannel imaging task
    iterates over 1 ROI list

--- logic/tasks/multichannel_serpentine_scan_task.py
+++ logic/tasks/multichannel_serpentine_scan_task.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Tests on implementing a serpentine scan task using the predefined task structure of qudi.
+Task allowing to iterate over an ROI list taking images in several channels (sequentially)
 
 
 Config example pour copy-paste:
-    serpentineScanTask:
-        module: 'serpentine_scan_task'
+    multichannelSerpentineScanTask:
+        module: 'multichannel_serpentine_scan_task'
         needsmodules:
             roi: 'roi_logic'
             daq: 'daq_ao_logic'
             camera: 'camera_logic'
             filter: 'filterwheel_logic'
         config:
-            path_to_user_config: '/home/barho/serpentine_scan_task_config.json'
+            path_to_user_config: '/home/barho/multichannel_scan_task_config.json'
 """
 
 from logic.generic_task import InterruptableTask
-# import time
+import time
 import json
 
 class Task(InterruptableTask): # do not change the name of the class. it is always called Task !
     """ This task does an acquisition of a series of images on the ROIs defined by the user
 
     current version: allows to iterate over 1 ROI list
-    using one light source with intensity specified in user config
+    using a sequence of light sources to be used
     """
 
     def __init__(self, **kwargs):
@@ -44,16 +44,9 @@ class Task(InterruptableTask): # do not change the name of the class. it is alwa
 
         # get the list of the roi names
         self.roi_names = self.ref['roi'].roi_names
-        #self.log.info(self.roi_names)
 
         # initialize the counter that will be used to iterate over the ROIs
         self.counter = 0  # counter for the number of task steps
-
-        # set the filter to the specified position
-        self.ref['filter'].set_position(self.filter_pos)
-
-        # indicate the intensity values to be applied
-        self.ref['daq'].update_intensity_dict(self.lightsource, self.intensity)
 
 
     def runTaskStep(self):
@@ -64,18 +57,26 @@ class Task(InterruptableTask): # do not change the name of the class. it is alwa
         self.ref['roi'].go_to_roi(name=self.roi_names[self.counter])
         self.log.info('Moved to {}'.format(self.roi_names[self.counter]))
 
-        # switch the laser on
-        self.ref['daq'].apply_voltage()
+        # iterate over the imaging sequence specified by the user
+        for key in self.imaging_sequence:
+            # set the filter to the specified position
+            self.ref['filter'].set_position(self.imaging_sequence[key]['filter_pos'])
 
-        # save an image - here: replaced by take image. save image needs to be implemented
-        # remember also to specify the save path in startTask method
-        self.ref['camera'].start_single_acquistion() # mind the typo !!
-        self.log.info('Saved an image into folder {}'.format(self.save_path)) # ... lets say we did ..
+            # indicate the intensity value to be applied to the lightsource
+            self.ref['daq'].update_intensity_dict(self.imaging_sequence[key]['lightsource'], self.imaging_sequence[key]['intensity'])
 
-        # switch laser off
-        self.ref['daq'].voltage_off()
+            # the following part has to be reworked when the synchronization between daq and camera is established
+            # and when saving functionality of camera is available
 
-        # in a following version the laser on off switching should be triggered by the camera. but this actually goes into the daq_ao_logic module ..
+            # switch the laser on
+            self.ref['daq'].apply_voltage()
+            # take an image
+            self.ref['camera'].start_single_acquistion() # mind the typo !!
+            # time.sleep(0.1)
+            # save an image # also think about the generic filename .. it must include the roi it belongs to, the channel, ..
+            self.log.info('Saved an image from channel {0} into folder {1}'.format(self.imaging_sequence[key]['lightsource'], self.save_path)) # ... lets say we did ..
+            # switch laser off
+            self.ref['daq'].voltage_off()
 
         self.counter += 1
         return self.counter < len(self.roi_names) # continue when there are still rois left in the list
@@ -103,9 +104,9 @@ class Task(InterruptableTask): # do not change the name of the class. it is alwa
         user must specify the following dictionary (here with example entries):
             roilist_path: '/home/barho/roilists/mylist.json'
             save_path: '/home/barho/myfolder'
-            lightsource: 'laser1'
-            intensity: 10
-            filter_pos: 2
+            imaging_sequence: {1: {'lightsource': 'laser2', 'intensity': 10, 'filter_pos': 2},
+                               2: {'lightsource': 'laser1', 'intensity': 30, 'filter_pos': 5}}
+
         """
         try:
             with open(self.user_config_path, 'r') as file:
@@ -113,9 +114,7 @@ class Task(InterruptableTask): # do not change the name of the class. it is alwa
 
             self.roi_path = self.user_param_dict['roilist_path']
             self.save_path = self.user_param_dict['save_path']
-            self.lightsource = self.user_param_dict['lightsource']
-            self.intensity = self.user_param_dict['intensity']
-            self.filter_pos = self.user_param_dict['filter_pos']
+            self.imaging_sequence = self.user_param_dict['imaging_sequence'] # which itself is a dictionary
 
         except:
             self.log.warning('Could not load user parameters for task {}'.format(self.name))
