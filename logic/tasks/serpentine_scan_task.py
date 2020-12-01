f262c59d3b33b778ee2e55d21e330c2d2cabe1aa
fbarho <franziska.barho@gmx.fr>
2020-12-01 13:17:37 +0100

    task with user parameters read from a separate config file

--- logic/tasks/serpentine_scan_task.py
+++ logic/tasks/serpentine_scan_task.py
@@ -12,42 +12,35 @@ Config example pour copy-paste:
             camera: 'camera_logic2'
             filter: 'filterwheel_logic'
         config:
-            roilist_path: '/home/barho/roilists/mylist.json'
-            save_path: '/home/barho/myfolder'
-            lightsource: 'laser1'
-            intensity: 10
-            filter_pos: 2
-
-
+            path_to_user_config: '/home/barho/serpentine_scan_task_config.json'
 """
 
 from logic.generic_task import InterruptableTask
-import time
+# import time
+import json
 
 class Task(InterruptableTask): # do not change the name of the class. it is always called Task !
     """ This task does an acquisition of a series of images on the ROIs defined by the user
 
-    current version: allows to iterate over 1 ROI list, path given in global config,
-    using one light source with intensity specified in the global config
+    current version: allows to iterate over 1 ROI list
+    using one light source with intensity specified in user config
     """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         print('Task {0} added!'.format(self.name))
-        self.log.info(self.config)
-
-        self.roi_path = self.config['roilist_path']
-        self.save_path = self.config['save_path']
-        self.lightsource = self.config['lightsource']
-        self.intensity = self.config['intensity']
-        self.filter_pos = self.config['filter_pos']
+        self.user_config_path = self.config['path_to_user_config']
+        self.log.info('Task {0} using the configuration at {1}'.format(self.name, self.user_config_path))
 
 
     def startTask(self):
         """ """
+        self._load_user_parameters()
+        self.log.info('loaded user parameters')
+
         # load a specified list in the ROI module
         self.ref['roi'].load_roi_list(self.roi_path)
-        self.log.info('loaded list')
+        self.log.info('loaded roi list')
 
         # get the list of the roi names
         self.roi_names = self.ref['roi'].roi_names
@@ -102,8 +95,34 @@ class Task(InterruptableTask): # do not change the name of the class. it is alwa
         self.ref['daq'].reset_intensity_dict()
         self.log.info('cleanupTask called')
 
-    def my_function(self):
-        """ """
-        self.log.info('called a custom function')
+    def _load_user_parameters(self):
+        """ this function is called from startTask() to load the parameters given in a specified format by the user
+
+        specify only the path to the user defined config in the (global) config of the experimental setup
+
+        user must specify the following dictionary (here with example entries):
+            roilist_path: '/home/barho/roilists/mylist.json'
+            save_path: '/home/barho/myfolder'
+            lightsource: 'laser1'
+            intensity: 10
+            filter_pos: 2
+        """
+        try:
+            with open(self.user_config_path, 'r') as file:
+                self.user_param_dict = json.load(file)
+
+            self.roi_path = self.user_param_dict['roilist_path']
+            self.save_path = self.user_param_dict['save_path']
+            self.lightsource = self.user_param_dict['lightsource']
+            self.intensity = self.user_param_dict['intensity']
+            self.filter_pos = self.user_param_dict['filter_pos']
+
+        except:
+            self.log.warning('Could not load user parameters for task {}'.format(self.name))
+
+
+
+
+
 
 
