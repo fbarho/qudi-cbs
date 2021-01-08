fixed error in data reshaping; image rotation issue and data conversion for export to tiff

gui/fluorescence_microscopy/basic_gui.py
@@ -263,7 +263,7 @@ class BasicGUI(GUIBase):
     def init_camera_dockwidget(self):
         """ initializes the display item and the indicators. Connects signals for the camera dockwidget and the camera toolbar"""
         # initialize the imageitem (display of camera image) qnd its histogram
-        self.imageitem = pg.ImageItem()  # image=data can be set here ..   axisOrder='row-major'
+        self.imageitem = pg.ImageItem(axisOrder='row-major', invertY=True) # image=data can be set here ..  axisOrder='row-major'
         self._mw.camera_ScanPlotWidget.addItem(self.imageitem)
         self._mw.camera_ScanPlotWidget.setAspectLocked(True)
         self._mw.camera_ScanPlotWidget.sigMouseAreaSelected.connect(self.mouse_area_selected)
@@ -629,14 +629,20 @@ class BasicGUI(GUIBase):
         Get the image data from the logic and print it on the window
         """
         image_data = self._camera_logic.get_last_image()
-        # handle the rotation settings
+        # handle the rotation that occurs due to the image formatting conventions (see also https://github.com/pyqtgraph/pyqtgraph/issues/315) 
+        # this could be improved by another method ?! though reversing the y axis did not work. 
+        image_data = np.rot90(image_data, 3)  # 90 deg clockwise 
+        
+        # handle the user defined rotation settings
         if self.rotation_cw:
             image_data = np.rot90(image_data, 3)
         if self.rotation_ccw:
             image_data = np.rot90(image_data, 1)  # eventually replace by faster rotation method T and invert
         if self.flip:  # rotation 180deg
             image_data = np.rot90(image_data, 2)
-        self.imageitem.setImage(image_data)  #  , axisOrder='row-major'
+        self.imageitem.setImage(image_data.T)  #
+        # transposing the data makes the rotations behave as they should when axisOrder row-major is used (set in initialization of ImageItem)
+        # see also https://github.com/pyqtgraph/pyqtgraph/issues/315
 
     @QtCore.Slot()
     def save_last_image_clicked(self):

hardware/camera/andor/iXon897_ultra.py
@@ -343,7 +343,7 @@ class IxonUltra(Base, CameraInterface):
             for i in range(len(cimage)):
                 image_array[i] = cimage[i]
 
-        image_array = np.reshape(image_array, (self._width, self._height))
+        image_array = np.reshape(image_array, (self._height, self._width))  # (self._width, self._height))
 
         return image_array
 
@@ -380,7 +380,7 @@ class IxonUltra(Base, CameraInterface):
         cimage_array = c_int * dim
         cimage = cimage_array()
 
-        # this will be a bit hacky
+        # this will be a bit hacky 
         if self._acquisition_mode == 'RUN_TILL_ABORT':
             # error_code = self.dll.GetOldestImage(pointer(cimage), dim)
             # new version for tests 
@@ -397,9 +397,9 @@ class IxonUltra(Base, CameraInterface):
 
         if self._scans > 1:  # not sure if it matters if we add artifically a 3rd dim, so use if-else structure for the moment
             image_array = np.reshape(image_array,
-                                     (self._scans, self._width, self._height))  # self._scans, self._width, self._height
+                                     (self._scans, self._height, self._width))  # self._scans, self._width, self._height
         else:
-            image_array = np.reshape(image_array, (self._width, self._height))
+            image_array = np.reshape(image_array, (self._height, self._width))
 
         self._cur_image = image_array
         return image_array

logic/camera_logic2.py
@@ -484,10 +484,13 @@ class CameraLogic(GenericLogic):
 
         @returns None
         """
+        # type conversion to int16
+        data = data.astype('int16')
+        # self.log.info('type conversion called')
         # 2D data case (no stack)
         if n_frames == 1:
             try:
-                self.save_u16_to_tiff(data, data.shape, path)
+                self.save_u16_to_tiff(data, (data.shape[1], data.shape[0]), path)
                 self.log.info('Saved data to file {}'.format(path))
             except:
                 self.log.warning('Data not saved')
@@ -496,7 +499,7 @@ class CameraLogic(GenericLogic):
         # 3D data (note: z stack is the first dimension)
         else:
             try:
-                size = (data.shape[1], data.shape[2])
+                size = (data.shape[2], data.shape[1])
                 self.save_u16_to_tiff_stack(n_frames, data, size, path)
                 self.log.info('Saved data to file {}'.format(path))
             except:
@@ -510,7 +513,8 @@ class CameraLogic(GenericLogic):
 
         modified version for numpy array only
 
-        @param u16int: np.array to be saved as tiff
+        @param u16int: np.array with dtype int16 to be saved as tiff. 
+             make sure that the data is in int16 format ! otherwise th conversion to bytes will not give the right result
         @param size: size of the data
         @param str tiff_filename
         """
@@ -524,7 +528,7 @@ class CameraLogic(GenericLogic):
     def save_u16_to_tiff_stack(self, n_frames, u16int, size, tiff_filename):
         """ handles saving of 3D image data to 16 bit tiff stacks
         @param int n_frames: number of frames to be saved (1st dimension of the image data)
-        @param u16int: 3D np.array to be saved as tiff
+        @param u16int: 3D np.array with dtype int16 to be saved as tiff
         @param int tuple size: size of an individual image in the stack (x pixel, y pixel)
         @param str tiff_filename: complete path to the file, including the suffix .tiff """
 