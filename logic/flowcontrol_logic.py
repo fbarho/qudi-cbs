4c950d0ca90f629940a9cdd17ee9d008a15eca5e
JB <jb.fiche@gmail.com>
2021-03-16 10:29:27 +0100

    update flowcontrol logic to handle multiplexed devices

--- logic/flowcontrol_logic.py
+++ logic/flowcontrol_logic.py
@@ -72,28 +72,83 @@ class FlowcontrolLogic(GenericLogic):
         """ Perform required deactivation. """
         pass
 
-    def get_pressure(self):
-        return self._pump.get_pressure()
+    def get_pressure(self, channels=None):
+        """
+        @param: list channels: optional, list of channels from which pressure value will be measured
+        """
+        pressure = self._pump.get_pressure(channels)  # returns a dictionary: {0: pressure_channel_0}
+        pressure = [*pressure.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(pressure) < 2:
+            return pressure[0]
+        else:
+            return pressure
 
-    def set_pressure(self, pressure):
-        self._pump.set_pressure(pressure)
-        # maybe add a message to log ..  or handle this at the hardware level ?
+    def set_pressure(self, pressures, channels=None):
+        """
+        @param: float or float list pressures: pressure to be set to a given channel
+        @param: int list channels: optional, needed in case more than one pressure channel is available
+        """
+        if not channels:
+            if not isinstance(pressures, float):  # a list is given
+                self.log.warning('Channels must be specified if more than one pressure value shall be set.')
+            else:
+                param_dict = {}
+                param_dict[0] = pressures  # maybe modify in case another pump has a different way of addressing its channel (adapt by config; default_channel_ID ?)
+                unit = self.get_pressure_unit()
+                self._pump.set_pressure(param_dict)
+                self.log.info(f'Pressure set to {pressures} {unit}')
+        else:
+            param_dict = dict(zip(channels, pressures))
+            self._pump.set_pressure(param_dict)
 
-    def get_pressure_range(self):
-        return self._pump.get_pressure()
+    def get_pressure_range(self, channels=None):
+        """
+        @param: list channels: optional, list of channels from which pressure range value will be retrieved
+        """
+        pressure_range = self._pump.get_pressure_range(channels)  # returns a dictionary: {0: pressure_range_channel_0}
+        pressure_range = [*pressure_range.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(pressure_range) < 2:
+            return pressure_range[0]
+        else:
+            return pressure_range
 
-    def get_pressure_unit(self):
-        return self._pump.get_pressure_unit()
+    def get_pressure_unit(self, channels=None):
+        """
+        @param: list channels: optional, list of channels from which pressure range unit will be retrieved
+        """
+        pressure_unit = self._pump.get_pressure_unit(channels)  # returns a dictionary: {0: pressure_unit_channel_0}
+        pressure_unit = [*pressure_unit.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(pressure_unit) < 2:
+            return pressure_unit[0]
+        else:
+            return pressure_unit
 
-    def get_flowrate(self):
-        return self._pump.get_flowrate()
+    def get_flowrate(self, channels=None):
+        """
+        @param: list channels: optional, list of channels from which pressure value will be measured
+        """
+        flowrate = self._pump.get_flowrate(channels)  # returns a dictionary: {0: flowrate_channel_0}
+        flowrate = [*flowrate.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(flowrate) < 2:
+            return flowrate[0]
+        else:
+            return flowrate
 
-    def get_flowrate_range(self):
-        return self._pump.get_sensor_range()
+    def get_flowrate_range(self, channels=None):
+        flowrate_range = self._pump.get_sensor_range(channels)  # returns a dictionary: {0: sensor_range_channel_0}
+        flowrate_range = [*flowrate_range.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(flowrate_range) < 2:
+            return flowrate_range[0]
+        else:
+            return flowrate_range
 
-    def get_flowrate_unit(self):
-        return self._pump.get_sensor_unit()
-
+    def get_flowrate_unit(self, channels=None):
+        flowrate_unit = self._pump.get_sensor_unit(channels)  # returns a dictionary: {0: sensor_unit_channel_0}
+        flowrate_unit = [*flowrate_unit.values()]  # retrieve only the values from the dictionary and convert into list
+        if len(flowrate_unit) < 2:
+            return flowrate_unit[0]
+        else:
+            return flowrate_unit
 
     def start_flow_measurement(self):
         self.measuring = True
