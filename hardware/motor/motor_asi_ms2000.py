6bbd0bd0595c170e2bb9600deebf7c58f0e9ec64
JB <jb.fiche@gmail.com>
2021-05-10 15:35:49 +0200

    added safety check for deconnection of asi stage

--- hardware/motor/motor_asi_ms2000.py
+++ hardware/motor/motor_asi_ms2000.py
@@ -11,7 +11,7 @@ obtained from <https://github.com/Ulm-IQO/qudi/>
 """
 
 import serial
-from time import sleep
+from time import sleep, time
 
 from core.module import Base
 from interface.motor_interface import MotorInterface
@@ -68,15 +68,22 @@ class MS2000(Base, MotorInterface, BrightfieldInterface):
             if isinstance(item, str):
                 self.axis_list.append(item)
 
-        return 0
 
     def on_deactivate(self):
         """ Close serial port when deactivating the module.
-        
-        @returns int: error code (ok: 0)
         """
         self._serial_connection.close()
-        return 0
+        # safety check  # to explore when problem with stage arises again ..
+        port_open = self._serial_connection.is_open
+        t0 = time()
+        while port_open:
+            print('in loop')
+            t1 = time() - t0
+            sleep(0.5)
+            # self._serial_connection.close()
+            port_open = self._serial_connection.is_open
+            if t1 > 5:
+                break
 
     def get_constraints(self):
         pass
