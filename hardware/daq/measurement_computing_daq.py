6cc402e4177650f2c6878b3080de063aaa23d0ea
unknown <franziska.barho@gmx.fr>
2021-06-07 15:58:24 +0200

    first draft mcc daq module

--- hardware/daq/measurement_computing_daq.py
+++ hardware/daq/measurement_computing_daq.py
@@ -10,19 +10,24 @@ This file contains a class for the Measurement Computing DAQ
 This module is an extension to the hardware code base of Qudi software
 obtained from <https://github.com/Ulm-IQO/qudi/>
 """
+import numpy as np
+from time import sleep
+from mcculw import ul
+from mcculw.enums import ULRange, DigitalIODirection, InterfaceType
+from mcculw.ul import ULError
+from mcculw.device_info import DaqDeviceInfo
 
 from core.module import Base
 from core.configoption import ConfigOption
-import numpy as np
-from time import sleep
 
 
-class McDAQ():
+
+class MccDAQ():
     """ Class representing the measurement computing DAQ.
 
     Example config for copy-paste:
-        mc_daq:
-            module.Class: 'daq.measurement_computing_daq.McDAQ'
+        mcc_daq:
+            module.Class: 'daq.measurement_computing_daq.MccDAQ'
 
     """
 
@@ -37,13 +42,261 @@ class McDAQ():
     def on_activate(self):
         """ Initialization steps when module is called.
         """
-        pass
+        ul.ignore_instacal()
+        devices = ul.get_daq_device_inventory(InterfaceType.ANY)
+        if not devices:
+            raise Exception('Error: No DAQ devices found')
+
+        print('Found', len(devices), 'DAQ device(s):')
+        for device in devices:
+            print('  ', device.product_name, ' (', device.unique_id, ') - ',
+                  'Device ID = ', device.product_id, sep='')
+
+        device = devices[0]
+        self.board_num = 0
+
+        # Add the first DAQ device to the UL with the specified board number
+        ul.create_daq_device(self.board_num, device)
 
     def on_deactivate(self):
         """ Required deactivation steps.
         """
+        ul.release_daq_device(self.board_num)
+
+    def write_to_ao_channel(self, voltage, channel):
+        daq_dev_info = DaqDeviceInfo(self.board_num)
+        if not daq_dev_info.supports_analog_output:
+            raise Exception('Error: The DAQ device does not support '
+                            'analog output')
+        ao_info = daq_dev_info.get_ao_info()
+        print(ao_info)
+        ao_range = ao_info.supported_ranges[0]
+        print(ao_range)
+
+        print('Outputting', voltage, 'Volts to channel', channel)
+        # # Send the value to the device (optional parameter omitted)
+        # ul.v_out(board_num, channel, ao_range, voltage)
+
+    # no ai channels for this daq
+    # def read_ai_channel(self):
+    #     daq_dev_info = DaqDeviceInfo(self.board_num)
+    #     if not daq_dev_info.supports_analog_input:
+    #         raise Exception('Error: The DAQ device does not support '
+    #                         'analog input')
+
+    def set_up_do_channel(self):
         pass
 
+    def write_to_do_channel(self):
+        pass
+
+    def set_up_di_channel(self):
+        pass
+
+    def read_di_channel(self):
+        pass
+
+    # def config_first_detected_device(self, board_num, dev_id_list=None):
+    #     """Adds the first available device to the UL.  If a types_list is specified,
+    #     the first available device in the types list will be add to the UL.
+    #     Parameters
+    #     ----------
+    #     board_num : int
+    #         The board number to assign to the board when configuring the device.
+    #     dev_id_list : list[int], optional
+    #         A list of product IDs used to filter the results. Default is None.
+    #         See UL documentation for device IDs.
+    #     """
+    #     ul.ignore_instacal()
+    #     devices = ul.get_daq_device_inventory(InterfaceType.ANY)
+    #     if not devices:
+    #         raise Exception('Error: No DAQ devices found')
+    #
+    #     print('Found', len(devices), 'DAQ device(s):')
+    #     for device in devices:
+    #         print('  ', device.product_name, ' (', device.unique_id, ') - ',
+    #               'Device ID = ', device.product_id, sep='')
+    #
+    #     device = devices[0]
+    #     print(f'device: {device}, {device.product_name}')
+    #     if dev_id_list:
+    #         device = next((device for device in devices
+    #                        if device.product_id in dev_id_list), None)
+    #         if not device:
+    #             err_str = 'Error: No DAQ device found in device ID list: '
+    #             err_str += ','.join(str(dev_id) for dev_id in dev_id_list)
+    #             raise Exception(err_str)
+    #
+    #     # Add the first DAQ device to the UL with the specified board number
+    #     ul.create_daq_device(board_num, device)
+
+    # def read_ai(self):
+    #     # By default, the example detects and displays all available devices and
+    #     # selects the first device listed. Use the dev_id_list variable to filter
+    #     # detected devices by device ID (see UL documentation for device IDs).
+    #     # If use_device_detection is set to False, the board_num variable needs to
+    #     # match the desired board number configured with Instacal.
+    #     use_device_detection = True
+    #     dev_id_list = []
+    #     board_num = 0
+    #
+    #     try:
+    #         if use_device_detection:
+    #             self.config_first_detected_device(board_num, dev_id_list)
+    #
+    #         daq_dev_info = DaqDeviceInfo(board_num)
+    #         if not daq_dev_info.supports_analog_input:
+    #             raise Exception('Error: The DAQ device does not support '
+    #                             'analog input')
+    #
+    #         print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
+    #               daq_dev_info.unique_id, ')\n', sep='')
+    #
+    #         ai_info = daq_dev_info.get_ai_info()
+    #         ai_range = ai_info.supported_ranges[0]
+    #         channel = 0
+    #
+    #         # Get a value from the device
+    #         if ai_info.resolution <= 16:
+    #             # Use the a_in method for devices with a resolution <= 16
+    #             value = ul.a_in(board_num, channel, ai_range)
+    #             # Convert the raw value to engineering units
+    #             eng_units_value = ul.to_eng_units(board_num, ai_range, value)
+    #         else:
+    #             # Use the a_in_32 method for devices with a resolution > 16
+    #             # (optional parameter omitted)
+    #             value = ul.a_in_32(board_num, channel, ai_range)
+    #             # Convert the raw value to engineering units
+    #             eng_units_value = ul.to_eng_units_32(board_num, ai_range, value)
+    #
+    #         # Display the raw value
+    #         print('Raw Value:', value)
+    #         # Display the engineering value
+    #         print('Engineering Value: {:.3f}'.format(eng_units_value))
+    #     except Exception as e:
+    #         print('\n', e)
+    #     finally:
+    #         if use_device_detection:
+    #             ul.release_daq_device(board_num)
+    #
+    # def read_di(self):
+    #     # By default, the example detects and displays all available devices and
+    #     # selects the first device listed. Use the dev_id_list variable to filter
+    #     # detected devices by device ID (see UL documentation for device IDs).
+    #     # If use_device_detection is set to False, the board_num variable needs to
+    #     # match the desired board number configured with Instacal.
+    #     use_device_detection = True
+    #     dev_id_list = []
+    #     board_num = 0
+    #
+    #     try:
+    #         if use_device_detection:
+    #             self.config_first_detected_device(board_num, dev_id_list)
+    #
+    #         daq_dev_info = DaqDeviceInfo(board_num)
+    #         if not daq_dev_info.supports_digital_io:
+    #             raise Exception('Error: The DAQ device does not support '
+    #                             'digital I/O')
+    #
+    #         print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
+    #               daq_dev_info.unique_id, ')\n', sep='')
+    #
+    #         dio_info = daq_dev_info.get_dio_info()
+    #
+    #         # Find the first port that supports input, defaulting to None
+    #         # if one is not found.
+    #         port = next((port for port in dio_info.port_info if port.supports_input),
+    #                     None)
+    #         if not port:
+    #             raise Exception('Error: The DAQ device does not support '
+    #                             'digital input')
+    #
+    #         # If the port is configurable, configure it for input.
+    #         if port.is_port_configurable:
+    #             ul.d_config_port(board_num, port.type, DigitalIODirection.IN)
+    #
+    #         # Get a value from the digital port
+    #         port_value = ul.d_in(board_num, port.type)
+    #
+    #         # Get a value from the first digital bit
+    #         bit_num = 0
+    #         bit_value = ul.d_bit_in(board_num, port.type, bit_num)
+    #
+    #         # Display the port value
+    #         print(port.type.name, 'Value:', port_value)
+    #         # Display the bit value
+    #         print('Bit', bit_num, 'Value:', bit_value)
+    #     except Exception as e:
+    #         print('\n', e)
+    #     finally:
+    #         if use_device_detection:
+    #             ul.release_daq_device(board_num)
+    # #
+    # def write_do():
+    #     # By default, the example detects and displays all available devices and
+    #     # selects the first device listed. Use the dev_id_list variable to filter
+    #     # detected devices by device ID (see UL documentation for device IDs).
+    #     # If use_device_detection is set to False, the board_num variable needs to
+    #     # match the desired board number configured with Instacal.
+    #     use_device_detection = True
+    #     dev_id_list = []
+    #     board_num = 0
+    #
+    #     try:
+    #         if use_device_detection:
+    #             self.config_first_detected_device(board_num, dev_id_list)
+    #
+    #         daq_dev_info = DaqDeviceInfo(board_num)
+    #         if not daq_dev_info.supports_digital_io:
+    #             raise Exception('Error: The DAQ device does not support '
+    #                             'digital I/O')
+    #
+    #         print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
+    #               daq_dev_info.unique_id, ')\n', sep='')
+    #
+    #         dio_info = daq_dev_info.get_dio_info()
+    #
+    #         # Find the first port that supports input, defaulting to None
+    #         # if one is not found.
+    #         port = next((port for port in dio_info.port_info if port.supports_output),
+    #                     None)
+    #         if not port:
+    #             raise Exception('Error: The DAQ device does not support '
+    #                             'digital output')
+    #
+    #         # If the port is configurable, configure it for output.
+    #         if port.is_port_configurable:
+    #             ul.d_config_port(board_num, port.type, DigitalIODirection.OUT)
+    #
+    #         port_value = 0xFF
+    #
+    #         print('Setting', port.type.name, 'to', port_value)
+    #
+    #         # Output the value to the port
+    #         ul.d_out(board_num, port.type, port_value)
+    #
+    #         bit_num = 0
+    #         bit_value = 0
+    #         print('Setting', port.type.name, 'bit', bit_num, 'to', bit_value)
+    #
+    #         # Output the value to the bit
+    #         ul.d_bit_out(board_num, port.type, bit_num, bit_value)
+    #
+    #     except Exception as e:
+    #         print('\n', e)
+    #     finally:
+    #         if use_device_detection:
+    #             ul.release_daq_device(board_num)
+
 if __name__ == '__main__':
-    mc_daq = McDAQ()
-    mc_daq.on_activate()
+    mcc_daq = MccDAQ()
+    mcc_daq.on_activate()
+    mcc_daq.write_to_ao_channel(0, 0)
+    # mcc_daq.read_ai_channel()
+    mcc_daq.on_deactivate()
+    # mcc_daq.config_first_detected_device(0)
+    # mcc_daq.read_ai()
+    # mcc_daq.read_di()
+    # mcc_daq.read_value()
+    # mcc_daq.run_example()
+    # mcc_daq.digital_in_example()
