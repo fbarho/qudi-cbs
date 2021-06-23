40494993923fdc6558ed1d63bd83704a53d45206
unknown <franziska.barho@gmx.fr>
2021-06-23 16:00:59 +0200

    added functions for controlling pump by mcc daq

--- hardware/daq/measurement_computing_daq.py
+++ hardware/daq/measurement_computing_daq.py
@@ -22,22 +22,27 @@ from core.configoption import ConfigOption
 
 
 
-class MccDAQ():
+class MccDAQ(Base):
     """ Class representing the measurement computing DAQ.
 
     Example config for copy-paste:
         mcc_daq:
             module.Class: 'daq.measurement_computing_daq.MccDAQ'
+            rinsing_pump_channel: 0
+            fluidics_pump_channel: 1
 
     """
 
     # config
+    # ao channels
+    rinsing_pump_channel = ConfigOption('rinsing_pump_channel', missing='error')
+    fluidics_pump_channel = ConfigOption('fluidics_pump_channel', missing='error')
 
-    # def __init__(self, config, **kwargs):
-    #     super().__init__(config=config, **kwargs)
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
 
     def on_activate(self):
         """ Initialization steps when module is called.
@@ -63,19 +68,24 @@ class MccDAQ():
         """
         ul.release_daq_device(self.board_num)
 
+#=======================================================================================================================
+# Helper functions
+#=======================================================================================================================
+
     def write_to_ao_channel(self, voltage, channel):
         daq_dev_info = DaqDeviceInfo(self.board_num)
         if not daq_dev_info.supports_analog_output:
             raise Exception('Error: The DAQ device does not support '
                             'analog output')
         ao_info = daq_dev_info.get_ao_info()
-        print(ao_info)
+        print(ao_info.supported_ranges)
         ao_range = ao_info.supported_ranges[0]
         print(ao_range)
 
         print('Outputting', voltage, 'Volts to channel', channel)
-        # # Send the value to the device (optional parameter omitted)
-        # ul.v_out(board_num, channel, ao_range, voltage)
+        # Send the value to the device (optional parameter omitted)
+        ul.v_out(self.board_num, channel, ao_range, voltage)
+
 
     # no ai channels for this daq
     # def read_ai_channel(self):
@@ -91,94 +101,38 @@ class MccDAQ():
         pass
 
     def set_up_di_channel(self):
-        pass
+        daq_dev_info = DaqDeviceInfo(self.board_num)
+        if not daq_dev_info.supports_digital_io:
+            raise Exception('Error: The DAQ device does not support digital I/O')
+
+        dio_info = daq_dev_info.get_dio_info()
+        print(dio_info)
+
+        print(f'port info: {dio_info.port_info}')
+        print([port for port in dio_info.port_info if port.supports_input])
+
+        # Find the first port that supports input, defaulting to None
+        # if one is not found.
+        # port = next((port for port in dio_info.port_info if port.supports_input), None)
+        # if not port:
+        #     raise Exception('Error: The DAQ device does not support digital input')
+        # print(port)
+
+        # If the port is configurable, configure it for input.
+        # if port.is_port_configurable:
+        #     ul.d_config_port(self.board_num, port.type, DigitalIODirection.IN)
 
     def read_di_channel(self):
-        pass
+        daq_dev_info = DaqDeviceInfo(self.board_num)
+        dio_info = daq_dev_info.get_dio_info()
+        port = next((port for port in dio_info.port_info if port.supports_input), None)
+        print(port)
+        # Get a value from the digital port
+        port_value = ul.d_in(self.board_num, port.type)
+        print(port_value)
 
-    # def config_first_detected_device(self, board_num, dev_id_list=None):
-    #     """Adds the first available device to the UL.  If a types_list is specified,
-    #     the first available device in the types list will be add to the UL.
-    #     Parameters
-    #     ----------
-    #     board_num : int
-    #         The board number to assign to the board when configuring the device.
-    #     dev_id_list : list[int], optional
-    #         A list of product IDs used to filter the results. Default is None.
-    #         See UL documentation for device IDs.
-    #     """
-    #     ul.ignore_instacal()
-    #     devices = ul.get_daq_device_inventory(InterfaceType.ANY)
-    #     if not devices:
-    #         raise Exception('Error: No DAQ devices found')
-    #
-    #     print('Found', len(devices), 'DAQ device(s):')
-    #     for device in devices:
-    #         print('  ', device.product_name, ' (', device.unique_id, ') - ',
-    #               'Device ID = ', device.product_id, sep='')
-    #
-    #     device = devices[0]
-    #     print(f'device: {device}, {device.product_name}')
-    #     if dev_id_list:
-    #         device = next((device for device in devices
-    #                        if device.product_id in dev_id_list), None)
-    #         if not device:
-    #             err_str = 'Error: No DAQ device found in device ID list: '
-    #             err_str += ','.join(str(dev_id) for dev_id in dev_id_list)
-    #             raise Exception(err_str)
-    #
-    #     # Add the first DAQ device to the UL with the specified board number
-    #     ul.create_daq_device(board_num, device)
 
-    # def read_ai(self):
-    #     # By default, the example detects and displays all available devices and
-    #     # selects the first device listed. Use the dev_id_list variable to filter
-    #     # detected devices by device ID (see UL documentation for device IDs).
-    #     # If use_device_detection is set to False, the board_num variable needs to
-    #     # match the desired board number configured with Instacal.
-    #     use_device_detection = True
-    #     dev_id_list = []
-    #     board_num = 0
-    #
-    #     try:
-    #         if use_device_detection:
-    #             self.config_first_detected_device(board_num, dev_id_list)
-    #
-    #         daq_dev_info = DaqDeviceInfo(board_num)
-    #         if not daq_dev_info.supports_analog_input:
-    #             raise Exception('Error: The DAQ device does not support '
-    #                             'analog input')
-    #
-    #         print('\nActive DAQ device: ', daq_dev_info.product_name, ' (',
-    #               daq_dev_info.unique_id, ')\n', sep='')
-    #
-    #         ai_info = daq_dev_info.get_ai_info()
-    #         ai_range = ai_info.supported_ranges[0]
-    #         channel = 0
-    #
-    #         # Get a value from the device
-    #         if ai_info.resolution <= 16:
-    #             # Use the a_in method for devices with a resolution <= 16
-    #             value = ul.a_in(board_num, channel, ai_range)
-    #             # Convert the raw value to engineering units
-    #             eng_units_value = ul.to_eng_units(board_num, ai_range, value)
-    #         else:
-    #             # Use the a_in_32 method for devices with a resolution > 16
-    #             # (optional parameter omitted)
-    #             value = ul.a_in_32(board_num, channel, ai_range)
-    #             # Convert the raw value to engineering units
-    #             eng_units_value = ul.to_eng_units_32(board_num, ai_range, value)
-    #
-    #         # Display the raw value
-    #         print('Raw Value:', value)
-    #         # Display the engineering value
-    #         print('Engineering Value: {:.3f}'.format(eng_units_value))
-    #     except Exception as e:
-    #         print('\n', e)
-    #     finally:
-    #         if use_device_detection:
-    #             ul.release_daq_device(board_num)
-    #
+
     # def read_di(self):
     #     # By default, the example detects and displays all available devices and
     #     # selects the first device listed. Use the dev_id_list variable to filter
@@ -288,11 +242,42 @@ class MccDAQ():
     #         if use_device_detection:
     #             ul.release_daq_device(board_num)
 
+# =======================================================================================================================
+# Methods for pump channels
+# =======================================================================================================================
+
+    def write_to_pump_ao_channel(self, voltage):
+        """ Start / Stop the needle rinsing pump
+
+        :param: float voltage: target voltage to apply to the channel
+
+        :return: None
+        """
+        if voltage >= 0 and voltage <= 10: # replace by reading limits from device
+            self.write_to_ao_channel(voltage, self.rinsing_pump_channel)
+        else:
+            self.log.warning('Voltage not in allowed range.')
+
+    def write_to_fluidics_pump_ao_channel(self, voltage):
+        if voltage >= 0 and voltage <= 10: # replace by reading limits from device
+            self.write_to_ao_channel(voltage, self.fluidics_pump_channel)
+        else:
+            self.log.warning('Voltage not in allowed range.')
+
+
+
 if __name__ == '__main__':
     mcc_daq = MccDAQ()
     mcc_daq.on_activate()
-    mcc_daq.write_to_ao_channel(0, 0)
-    # mcc_daq.read_ai_channel()
+    # mcc_daq.write_to_ao_channel(0, 0)
+    mcc_daq.write_to_pump_ao_channel(1)
+    sleep(2)
+    mcc_daq.write_to_pump_ao_channel(0)
+
+    mcc_daq.write_to_fluidics_pump_ao_channel(1)
+    sleep(2)
+    mcc_daq.write_to_fluidics_pump_ao_channel(0)
+
     mcc_daq.on_deactivate()
     # mcc_daq.config_first_detected_device(0)
     # mcc_daq.read_ai()
